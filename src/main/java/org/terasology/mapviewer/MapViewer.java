92bddc11567dcec8ee570dc194448c6313ff8ecd
Martin Steiger <git@martin-steiger.de>
2014-10-17 18:45:44 +0200

    Add support for git-based versioning

--- src/main/java/org/terasology/mapviewer/MapViewer.java
+++ src/main/java/org/terasology/mapviewer/MapViewer.java
@@ -21,21 +21,36 @@ import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import version.GitVersion;
+
 /**
  * Preview generated world in Swing
  * @author Martin Steiger
  */
-final class MapViewer {
+public final class MapViewer {
+
+    private static final Logger logger = LoggerFactory.getLogger(MapViewer.class);
 
     private MapViewer() {
-
+        // don't create instances
     }
 
     public static void main(String[] args) throws IOException {
 
+        logger.info("Starting ...");
+
+        logger.debug("Java: {} {} {}", System.getProperty("java.version"), System.getProperty("java.vendor"), System.getProperty("java.home"));
+        logger.debug("Java VM: {} {} {}", System.getProperty("java.vm.name"), System.getProperty("java.vm.vendor"), System.getProperty("java.vm.version"));
+        logger.debug("Java classpath: {}", System.getProperty("java.class.path"));
+        logger.debug("OS: {} {} {}", System.getProperty("os.name"), System.getProperty("os.arch"), System.getProperty("os.version"));
+        logger.debug("Max. Memory: {} bytes", Runtime.getRuntime().maxMemory());
+
         final JFrame frame = new MainFrame();
 
-        frame.setTitle("MapViewer");
+        frame.setTitle("MapViewer " + GitVersion.getVersion());
         frame.setVisible(true);
         frame.setSize(2 * 512 + 40, 512 + 60);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
