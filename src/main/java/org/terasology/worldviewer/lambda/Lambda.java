96ff8763f55a1c38b0e6cc3df8d8fc100d3d1685
Martin Steiger <git@martin-steiger.de>
2015-03-07 13:17:51 +0100

    Cleanup lambda package

--- src/main/java/org/terasology/worldviewer/lambda/Lambda.java
+++ src/main/java/org/terasology/worldviewer/lambda/Lambda.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.terasology.worldviewer.lambda;
 
 import java.util.function.Consumer;
@@ -6,14 +22,13 @@ import java.util.function.Supplier;
 /**
  * @author Martin Steiger
  */
-public final class Lambda
-{
+public final class Lambda {
+
     private Lambda() {
         // no instances
     }
 
-    public static <T> Supplier<T> toRuntime(CheckedSupplier<T> checkedSupp)
-    {
+    public static <T> Supplier<T> toRuntime(CheckedSupplier<T> checkedSupp) {
         return () -> {
             try {
                 return checkedSupp.get();
@@ -23,8 +38,7 @@ public final class Lambda
         };
     }
 
-    public static <T> Consumer<T> toRuntime(CheckedConsumer<T> checkedConsumer)
-    {
+    public static <T> Consumer<T> toRuntime(CheckedConsumer<T> checkedConsumer) {
         return (t) -> {
             try {
                 checkedConsumer.accept(t);
